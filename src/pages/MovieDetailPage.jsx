1e4df99d5bcbca9d7b32b3c2e5da6e4aaa0ff870
brycerobinsn <bryce.robinson@gmail.com>
2022-11-30 00:30:19 -0800

    add movie detail page

--- src/pages/MovieDetailPage.jsx
+++ src/pages/MovieDetailPage.jsx
@@ -1,5 +1,18 @@
+import { movies } from "../data"
+import { useParams } from "react-router-dom"
+
 export default function MovieDetailPage(){
+    const movieTitle = useParams()
+    const movie = movies.find(movie => movie.title === movieTitle.movieTitle )
+
     return (
-        <h1>MovieDetailPage</h1>
+        <>
+            <h1>MovieDetailPage</h1>
+            <h2>{movie.title}</h2>
+            <div>Released: {movie.releaseDate}</div>
+            <div>Cast: {movie.cast.join(', ')}</div>
+            <img src={movie.posterPath}/>
+        </>
+
     )
 }
